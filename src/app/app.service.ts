043172663ca5e9cefd6aec54c0654d02a43546cb
Rody Davis <rody.davis.jr@gmail.com>
2020-03-31 16:45:33 -0700

    adding

---
+++ src/app/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+
+import {
+  AngularFirestoreDocument,
+  AngularFirestore,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class AppService {
+  constructor() {}
+}
